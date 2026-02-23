d13ffb8a6a088c8e552d5d83cc311e03daf9c993
mirillazhu <mira.zhu@gmail.com>
2026-02-23 18:17:22 -0500

    removed api dashboard components

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,11 +1,9 @@
-import datetime
-from collections import defaultdict, Counter
 from django.contrib.auth.decorators import user_passes_test
 from django.db.models import Count, F
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from allauth.socialaccount.models import SocialAccount
-from profiles.models import Subscription, Notification, UsageReport, Profile, KEY_TIERS
+from profiles.models import Subscription, Notification
 from utils.common import abbr_to_jid, sessions_with_bills, states
 from utils.orgs import get_chambers_from_abbr
 from dashboards.models import DataQualityReport
@@ -167,49 +165,3 @@ def _counter_to_chartdata(counter):
             cur_item[k] = v
         ret_data.append(cur_item)
     return sorted(ret_data, key=lambda x: x["date"])
-
-
-@user_passes_test(lambda u: u.is_superuser)
-def api_overview(request):
-    endpoint_usage = defaultdict(lambda: defaultdict(int))
-    key_usage = defaultdict(lambda: defaultdict(int))
-    key_totals = Counter()
-    v2_key_totals = Counter()
-    v3_key_totals = Counter()
-    all_keys = set()
-
-    days = int(request.GET.get("days", 60))
-    since = datetime.datetime.today() - datetime.timedelta(days=days)
-
-    reports = list(
-        UsageReport.objects.filter(date__gte=since, calls__gt=0).select_related(
-            "profile__user"
-        )
-    )
-    for report in reports:
-        date = str(report.date)
-        key = f"{report.profile.api_key} - {report.profile.user.email}"
-        endpoint_usage[date][report.endpoint] += report.calls
-        key_usage[date][key] += report.calls
-        key_totals[key] += report.calls
-        if report.endpoint == "graphql":
-            v2_key_totals[key] += report.calls
-        elif report.endpoint == "v3":
-            v3_key_totals[key] += report.calls
-        all_keys.add(key)
-
-    context = {
-        "endpoint_usage": _counter_to_chartdata(endpoint_usage),
-        "key_usage": _counter_to_chartdata(key_usage),
-        "most_common": key_totals.most_common(),
-        "v2_totals": v2_key_totals,
-        "v3_totals": v3_key_totals,
-        "key_tiers": list(KEY_TIERS.values()),
-        "total_keys": Profile.objects.exclude(
-            api_tier__in=("inactive", "suspended")
-        ).count(),
-        "active_keys": len(all_keys),
-        "days": days,
-    }
-
-    return render(request, "dashboards/api.html", {"context": context})
