352742dd5fa6e61583e3cde22f8698d0b1bcfde1
mirillazhu <mira.zhu@gmail.com>
2026-03-06 20:44:46 -0500

    comment out unused urls

--- public/urls.py
+++ public/urls.py
@@ -73,7 +73,7 @@ urlpatterns = [
     # for development only -- remove for production
     path("development-pdf-proxy/<path:remote_url>", document_proxy_for_development),
     # fallbacks
-    path("reportcard/", fallback),
+    # path("reportcard/", fallback),
     re_path(r"[a-z]{2}/votes/[A-Z]{2}V\d{8}/$", fallback),
     re_path(
         r"[a-z]{2}/legislators/(?P<legislator_id>[A-Z]{2}L\d{6})/", legislator_fallback
