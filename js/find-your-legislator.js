d04cc0ca65bdbd0e3adf61c6146d025db84be13f
mirillazhu <mira.zhu@gmail.com>
2026-08-29 23:23:41 -0400

    add visually hidden label to forms

--- js/find-your-legislator.js
+++ js/find-your-legislator.js
@@ -291,6 +291,12 @@ export default class FindYourLegislator extends React.Component {
              Find out who represents you by entering your address below:
           </h2>
           <div className="input-group">
+            <label
+              className="show-for-sr"
+              for="fyl-address"
+            >
+              Address
+            </label>
             <input
               className="input-group-field"
               type="search"
