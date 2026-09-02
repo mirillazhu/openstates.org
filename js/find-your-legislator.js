f554f0882049270101b5df9795c1d432244f9c05
mirillazhu <mira.zhu@gmail.com>
2026-09-02 15:17:48 -0400

    underline links in paragraphs

--- js/find-your-legislator.js
+++ js/find-your-legislator.js
@@ -277,8 +277,8 @@ export default class FindYourLegislator extends React.Component {
             </div>
           </div>
 
-          <div className="mapbox-credit">
-              In most cases, this should be your pre-incarceration address. Learn more <a href="/resources/">here</a>.
+          <div className="mb2">
+              <p>In most cases, this should be your pre-incarceration address. Learn more with <a href="/resources/">Frequently Asked Questions</a>.</p>
           </div>
 
           {this.state.error ? ( 
