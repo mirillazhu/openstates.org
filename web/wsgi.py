ddc23e87377d003ff8b518f234b9d19fe6edad2f
mirillazhu <mira.zhu@gmail.com>
2026-09-04 00:01:49 -0400

    temporarily deactivate new relic

--- web/wsgi.py
+++ web/wsgi.py
@@ -2,13 +2,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-try:
-    import newrelic.agent
+# try:
+#    import newrelic.agent
 
-    newrelic.agent.initialize()
-    newrelic.agent.capture_request_params()
-except Exception as e:
-    print("newrelic couldn't be initialized:", e)
+#    newrelic.agent.initialize()
+#    newrelic.agent.capture_request_params()
+# except Exception as e:
+#    print("newrelic couldn't be initialized:", e)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
